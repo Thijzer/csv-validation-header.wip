3f686c97661fc2e78224b10f1718c40456c4ba94
Ruben Van den Abbeele <rubenvda@hotmail.com>
2020-02-27 17:03:16 +0100

    InArrayValidator now cheks if given value is an array, if array it loops over the items instead of getting stuck

--- src/Component/Validator/InArrayValidator.php
+++ src/Component/Validator/InArrayValidator.php
@@ -7,21 +7,49 @@ use Misery\Component\Common\Options\OptionsTrait;
 
 class InArrayValidator extends AbstractValidator implements OptionsInterface
 {
+    /** @var string */
     public const NAME = 'in_array';
 
     use OptionsTrait;
 
+    /** @var array */
     private $options = [
         'options' => [],
     ];
 
+    /** @var array */
+    private $context = [];
+
+    /** @inheritDoc */
     public function validate($value, array $context = []): void
+    {
+        $this->context = $context;
+
+        if(is_array($value))
+        {
+            $this->validateArray($value);
+        }
+        else
+        {
+            $this->validateValue($value);
+        }
+    }
+
+    private function validateArray(array $array): void
+    {
+        foreach($array as $value)
+        {
+            $this->validateValue($value);
+        }
+    }
+
+    private function validateValue($value): void
     {
         if (!empty($this->options['options']) && !\in_array($value, $this->options['options'], true)) {
             $this->getValidationCollector()->collect(
                 new Constraint\InArrayConstraint(),
                 sprintf(Constraint\InArrayConstraint::NOT_LISTED, $value),
-                $context
+                $this->context
             );
         }
     }
