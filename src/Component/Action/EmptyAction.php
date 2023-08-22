ebc0d8b94f6fc3882c86e1e561e36e1ed927bd4f
Thomas Brankaer <thomas.brankaer@induxx.be>
2023-08-22 14:37:02 +0200

    Bugfix empty action

--- src/Component/Action/EmptyAction.php
+++ src/Component/Action/EmptyAction.php
@@ -31,23 +31,25 @@ class EmptyAction implements OptionsInterface, ConfigurationAwareInterface
             return $item;
         }
 
-        if (!empty($list)) {
-            foreach ($list as $key) {
-                if (isset($item[$field][$key]) && is_array($item[$field][$key])) {
-                    foreach ($item[$field][$key] as $index => $value) {
-                        if (is_array($value) && isset($value['data'])) {
-                            $item[$field][$key][$index]['data'] = null;
-                        }
-                    }
-                }
-            }
-        }
-
         if (!empty($prefix) && isset($item[$field]) && is_array($item[$field])) {
             foreach ($item[$field] as $key => $values) {
                 if (is_array($values) && substr($key, 0, strlen($prefix)) === $prefix && ($list && !in_array($key, $list))) {
                     foreach ($values as $index => $value) {
                         if (isset($value['data'])) {
+                            $item[$field][$key][$index]['data'] = null;
+                        }
+                    }
+                }
+            }
+
+            return $item;
+        }
+
+        if (!empty($list)) {
+            foreach ($list as $key) {
+                if (isset($item[$field][$key]) && is_array($item[$field][$key])) {
+                    foreach ($item[$field][$key] as $index => $value) {
+                        if (is_array($value) && isset($value['data'])) {
                             $item[$field][$key][$index]['data'] = null;
                         }
                     }
