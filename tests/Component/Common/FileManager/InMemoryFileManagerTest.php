45c568664ffd6d7236dacc0ca651900e12b2e871
Thijs De Paepe <thijs.dp@gmail.com>
2023-08-31 11:07:17 +0200

    added: inMemoryFileManagerTest.php

---
+++ tests/Component/Common/FileManager/InMemoryFileManagerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Component\Common\FileManager;
+
+use Misery\Component\Common\FileManager\InMemoryFileManager;
+use PHPUnit\Framework\TestCase;
+
+class InMemoryFileManagerTest extends TestCase
+{
+    public function testAddFiles()
+    {
+        $fileManager = new InMemoryFileManager();
+        $files = [
+            'file1.txt' => __DIR__ . '/../../../examples/dummies/file1.txt',
+            'file2.txt' => __DIR__ . '/../../../examples/dummies/file2.txt',
+        ];
+
+        $fileManager->addFiles($files);
+
+        $this->assertCount(2, $fileManager->getAliases());
+        $this->assertEquals(__DIR__ .'/../../../examples/dummies/file1.txt', $fileManager->getFile('file1.txt'));
+        $this->assertEquals(__DIR__ .'/../../../examples/dummies/file2.txt', $fileManager->getFile('file2.txt'));
+    }
+
+    public function testAddAliases()
+    {
+        $fileManager = new InMemoryFileManager();
+        $files = [
+            'file1.txt' => __DIR__ . '/../../../examples/dummies/file1.txt',
+            'file2.txt' => __DIR__ . '/../../../examples/dummies/file2.txt',
+        ];
+        $aliases = [
+            'alias1' => 'file1.*',
+            'alias2' => 'file1.txt',
+        ];
+
+        $fileManager->addFiles($files);
+        $fileManager->addAliases($aliases);
+
+        $this->assertCount(4, $fileManager->getAliases());
+        $this->assertEquals(__DIR__ .'/../../../examples/dummies/file1.txt', $fileManager->getFile('alias1'));
+        $this->assertEquals(__DIR__ .'/../../../examples/dummies/file1.txt', $fileManager->getFile('alias2'));
+        $this->assertEquals(__DIR__ .'/../../../examples/dummies/file1.txt', $fileManager->getFile('file1.txt'));
+    }
+}
