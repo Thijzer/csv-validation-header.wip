560d292b54c8afc9935f5eb2bab9c763887dec68
Thijs De Paepe <thijs.dp@gmail.com>
2023-09-29 15:35:37 +0200

    added: setAction support for std data fields

--- src/Component/Action/SetValueAction.php
+++ src/Component/Action/SetValueAction.php
@@ -4,6 +4,7 @@ namespace Misery\Component\Action;
 
 use Misery\Component\Common\Options\OptionsInterface;
 use Misery\Component\Common\Options\OptionsTrait;
+use Misery\Component\Converter\Matcher;
 
 class SetValueAction implements ActionInterface, OptionsInterface
 {
@@ -14,13 +15,41 @@ class SetValueAction implements ActionInterface, OptionsInterface
     /** @var array */
     private $options = [
         'key' => null,
+        'field' => null,
         'value' => null,
     ];
 
     public function apply(array $item): array
     {
-        $item[$this->options['key']] = $this->options['value'];
+        $field = $this->getOption('field', $this->getOption('key'));
+        $value = $this->getOption('value');
+
+        $key = $this->findMatchedValueData($item, $field);
+        if ($key) {
+            $item[$key]['data'] = $value;
+
+            return $item;
+        }
+
+        // don't allow array expansion when the field is not found
+        // use the expand function for this
+        if (key_exists($field, $item)) {
+            $item[$field] = $value;
+        }
 
         return $item;
+    }
+
+    private function findMatchedValueData(array $item, string $field): int|string|null
+    {
+        foreach ($item as $key => $itemValue) {
+            $matcher = $itemValue['matcher'] ?? null;
+            /** @var $matcher Matcher */
+            if ($matcher && $matcher->matches($field)) {
+                return $key;
+            }
+        }
+
+        return null;
     }
 }
--- tests/Component/Action/SetValueActionTest.php
+++ tests/Component/Action/SetValueActionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Misery\Component\Action;
 
 use Misery\Component\Action\SetValueAction;
+use Misery\Component\Converter\Matcher;
 use PHPUnit\Framework\TestCase;
 
 class SetValueActionTest extends TestCase
@@ -31,7 +32,7 @@ class SetValueActionTest extends TestCase
         ], $format->apply($item));
     }
 
-    public function test_it_should_set_a_new_value_action(): void
+    public function test_it_should_not_set_a_new_value_action(): void
     {
         $format = new SetValueAction();
 
@@ -50,7 +51,100 @@ class SetValueActionTest extends TestCase
             'brand' => 'louis',
             'description' => 'LV',
             'sku' => '1',
+        ], $format->apply($item));
+    }
+
+    public function test_it_should_set_a_null_value_action(): void
+    {
+        $format = new SetValueAction();
+
+        $item = [
+            'brand' => 'louis',
+            'description' => 'LV',
+            'sku' => '1',
             'published' => '1',
+        ];
+
+        $format->setOptions([
+            'field' => 'published',
+            'value' => null,
+        ]);
+
+        $this->assertEquals([
+            'brand' => 'louis',
+            'description' => 'LV',
+            'sku' => '1',
+            'published' => null,
+        ], $format->apply($item));
+    }
+
+    public function test_it_should_set_fields_std_data()
+    {
+        $format = new SetValueAction();
+
+        $item = [
+            'values|street' => [
+                'matcher' => Matcher::create('street'),
+                'scope' => null,
+                'locale' => null,
+                'data' => '123 Main Street',
+            ],
+        ];
+
+        $format->setOptions([
+            'field' => 'street',
+            'value' => null
+        ]);
+
+        $this->assertEquals([
+            'values|street' => [
+                'matcher' => Matcher::create('street'),
+                'scope' => null,
+                'locale' => null,
+                'data' => null,
+            ],
+        ], $format->apply($item));
+
+        $format->setOptions([
+            'field' => 'street',
+            'value' => 'unknown',
+        ]);
+
+        $this->assertEquals([
+            'values|street' => [
+                'matcher' => Matcher::create('street'),
+                'scope' => null,
+                'locale' => null,
+                'data' => 'unknown',
+            ],
+        ], $format->apply($item));
+    }
+
+    public function test_it_should_not_set_fields_std_data()
+    {
+        $format = new SetValueAction();
+
+        $item = [
+            'values|street' => [
+                'matcher' => Matcher::create('street'),
+                'scope' => null,
+                'locale' => null,
+                'data' => '123 Main Street',
+            ],
+        ];
+
+        $format->setOptions([
+            'field' => 'zone',
+            'value' => 'urban'
+        ]);
+
+        $this->assertEquals([
+            'values|street' => [
+                'matcher' => Matcher::create('street'),
+                'scope' => null,
+                'locale' => null,
+                'data' => '123 Main Street',
+            ],
         ], $format->apply($item));
     }
 }
